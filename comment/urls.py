080b9d94ec19164ed64afaf77c16798fe790edaa
elijahrosett <rosett3@gmail.com>
2022-05-11 12:03:16 -0700

    Created comment url paths + functions to handle get and post in views folder

--- comment/urls.py
+++ comment/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from comment import views
 
-# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
 
 urlpatterns = [
     path('', views.user_comments),
